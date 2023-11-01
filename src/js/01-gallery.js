22f3e1b111d30fa054390a8482d2def9c31bb482
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2023-11-01 21:08:56 +0200

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
